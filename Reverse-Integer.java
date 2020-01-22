4ad4c115b0d49f6bcd2229227a1961ae1294673b
khizar-s <59326221+khizar-s@users.noreply.github.com>
2020-01-22 08:54:56 -0500

    Create Reverse-Integer.java

---
+++ Reverse-Integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int reverse(int x) {
+        boolean negativeFlag = false; 
+        if (x < 0) 
+        { 
+            negativeFlag = true; 
+            x = -x ; 
+        } 
+       
+        int prev_rev_num = 0, rev_num = 0; 
+        while (x != 0) 
+        { 
+            int curr_digit = x%10; 
+            rev_num = (rev_num*10) + curr_digit; 
+            if ((rev_num - curr_digit)/10 != prev_rev_num) 
+            { 
+                return 0; 
+            } 
+       
+            prev_rev_num = rev_num; 
+            x = x/10; 
+        } 
+        return (negativeFlag == true)? -rev_num : rev_num; 
+    }
+}
