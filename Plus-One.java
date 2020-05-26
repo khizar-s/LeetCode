b5c1b02daffbecb6bcd208eed2046ad24e742656
khizar-s <59326221+khizar-s@users.noreply.github.com>
2020-05-25 22:09:41 -0400

    Create Plus-One.java

---
+++ Plus-One.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int l = digits.length - 1;
+        for (int i = l; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i] += 1;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
