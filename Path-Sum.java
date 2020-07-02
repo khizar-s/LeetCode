675988b3769220b3fc1bd3dcba6a715533bd6c00
khizar <59326221+khizar-s@users.noreply.github.com>
2020-07-01 23:15:40 -0400

    Create Path-Sum.java

---
+++ Path-Sum.java
@@ -0,0 +1,24 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        } else if (root.left == null && root.right == null && (sum == root.val)) {
+            return true;
+        } else {
+            return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+    }
+}
