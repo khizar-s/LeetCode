08bd481ec4692fedd66001164a27ff39543d9abd
Khizar <khizarce@gmail.com>
2021-06-11 22:14:53 -0400

    Enhancements

--- Climbing-Stairs.java
+++ Climbing-Stairs.java
@@ -1,16 +1,22 @@
 class Solution {
     public int climbStairs(int n) {
+        
         if (n == 1) {
             return 1;
         }
-        int first = 1;
-        int second = 2;
-        for (int i = 3; i <= n; i++) {
-            int third = first + second;
-            first = second;
-            second = third;
+        
+        if (n == 2) {
+            return 2;
         }
         
-        return second;
+        int res[] = new int[n];
+        res[0] = 1;
+        res[1] = 2;
+        
+        for (int i = 2; i < n; i++ ) {
+            res[i] = res[i-1] + res[i-2];
+        }
+        
+        return res[n-1];
     }
 }
