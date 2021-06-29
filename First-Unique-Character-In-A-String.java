15bdf16978177c03dc9d552ea5874fd2aeb3fa96
Khizar <khizarce@gmail.com>
2021-06-28 23:22:54 -0400

    Enhancements

--- First-Unique-Character-In-A-String.java
+++ First-Unique-Character-In-A-String.java
@@ -1,28 +1,20 @@
-import java.util.HashMap;
-
 class Solution {
     public int firstUniqChar(String s) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        for (int i = 0; i < s.length(); i++) {
-            char current = s.charAt(i);
-            if (!map.containsKey(current)) {
-                map.put(current, i);
-            } else {
-                map.put(current, -1);
+        
+        char[] arr = s.toCharArray();
+        int[]  occ = new int[26];
+        
+        for (char c : arr) {
+            occ[c-'a']++;
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (occ[arr[i]-'a'] == 1) {
+                return i;
             }
         }
+                
+        return -1;        
         
-        int min = Integer.MAX_VALUE;
-        for (char c: map.keySet()) {
-            if (map.get(c) > -1 && map.get(c) < min) {
-                min = map.get(c);
-            } 
-        }
-        
-        if (min == Integer.MAX_VALUE) {
-            return -1;
-        } else {
-            return min;
-        }
     }
 }
