4fbb1fdfd9b55fbfe3e5c75c46329a2a1b6e6031
Khizar <khizarce@gmail.com>
2021-07-02 23:40:05 -0400

    Enhancements

--- Longest-Common-Prefix.java
+++ Longest-Common-Prefix.java
@@ -1,20 +1,34 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String result = "";
-        if (strs == null || strs.length == 0) {
-            return result;
-        }
         
-        int index = 0;
-        for (char c : strs[0].toCharArray()) {
-            for (int i = 1; i < strs.length; i++) {
-                if (index >= strs[i].length() || c != strs[i].charAt(index)) {
-                    return result;
+        String first  = strs[0];
+        Boolean end   = false;
+        Boolean track = true;
+        int index     = 0;
+        String res    = "";
+        
+        while (true) {
+            if (end == true) {
+                return res;
+            } else {
+                for(String s : strs) {
+                    if (s.length() == 0) {
+                        return res;
+                    }
+                    if (index == s.length()-1) {
+                        end = true;
+                    }
+                    if (s.charAt(index) != first.charAt(index)) {
+                        track = false;
+                    }
+                }
+                if (track == true) {
+                    res += first.charAt(index);
+                    index += 1;
+                } else {
+                    return res;
                 }
             }
-            result += c;
-            index++;
         }
-        return result;
     }
 }
